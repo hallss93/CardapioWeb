4f308124ffb3770db42419065f13cf752fa72d8c
Hallison <hallss93@hotmail.com>
2017-03-05 22:48:41 -0300

    faltando validações

--- assets/js/jS.js
+++ assets/js/jS.js
@@ -1,5 +1,4 @@
-function adicionar(){
-console.log("inserindo");
+function adicionar() {
   var nome = document.getElementById('nome');
   var valor = document.getElementById('valor');
   var descricao = document.getElementById('descricao');
@@ -7,20 +6,22 @@ console.log("inserindo");
 
   var newPostKey = firebase.database().ref().child('cardapio/').push().key;
   var fruitRef = database.ref('cardapio/' + newPostKey);
-
-console.log("inserindo logando");
-firebase.auth().signInWithEmailAndPassword('1234@hotmail.com', '123456')
-.then(function(user) {
-
-
-  console.log("logado ");
-    var result = fruitRef.set(
-    {
-      nome: nome.value,
-      valor:valor.value,
-      descricao: descricao.value
-    });
-    console.log("usuario ==>" + user);
-    alert("Cadastro do Cardápio '" + nome.value + "' concluído com sucesso!")
-})
+  if (nome.value != "" && valor.value != "" && descricao.value != "") {
+    firebase.auth().signInWithEmailAndPassword('1234@hotmail.com', '123456')
+      .then(function (user) {
+        console.log("logado ");
+        var result = fruitRef.set(
+          {
+            nome: nome.value,
+            valor: valor.value,
+            descricao: descricao.value
+          });
+        alert("Cadastro do Cardápio '" + nome.value + "' concluído com sucesso!")
+      })
+  } else alert("Preencha todos os campos!");
+}
+function limpar() {
+  document.getElementById('nome').value = "";
+  document.getElementById('valor').value = "";
+  document.getElementById('descricao').value = "";
 }
